52d21ad001489ccd18df97fc49ac8f4fb7e63979
Jiho Lee <optional.int@kakao.com>
2023-01-09 13:12:22 +0900

    feat: wsl neovim discord ipc adaptation

---
+++ init-discord-rpc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+pkill socat
+rm -f /var/run/discord-ipc-0
+
+socat UNIX-LISTEN:/var/run/discord-ipc-0,fork \
+    EXEC:"npiperelay.exe //./pipe/discord-ipc-0" &
+
+sleep 2
+chmod 777 /var/run/discord-ipc-0
