a1d77476e82f1c73f557efbca2a12f6a852f5495
Jiho Lee <optional.int@kakao.com>
2023-04-20 11:47:12 +0900

    chore: silent rclone output

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,3 @@
 mkdir ~/pdf ~/논문 > /dev/null 2>&1
-nohup rclone mount onedrive:pdf ~/pdf &
-nohup rclone mount onedrive:논문 ~/논문 &
+nohup rclone mount onedrive:pdf ~/pdf > /dev/null 2>&1 &
+nohup rclone mount onedrive:논문 ~/논문 > /dev/null 2>&1 &
