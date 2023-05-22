46ac5f80206f316cdd1389dc866345158ebac9d1
Jiho Lee <optional.int@kakao.com>
2023-05-22 20:53:54 +0900

    feat: add auth folder on rclone

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,4 @@
-mkdir ~/pdf ~/논문 > /dev/null 2>&1
+mkdir ~/pdf ~/논문 ~/auth > /dev/null 2>&1
 nohup rclone mount onedrive:pdf ~/pdf > /dev/null 2>&1 &
 nohup rclone mount onedrive:논문 ~/논문 > /dev/null 2>&1 &
+nohup rclone mount onedrive:auth ~/auth > /dev/null 2>&1 &
