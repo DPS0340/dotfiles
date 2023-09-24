204d8bd32259eb73b4ddf0d43e12e58936d0ddbf
Jiho Lee <optional.int@kakao.com>
2023-09-25 05:19:57 +0900

    feat: use sudo pacman

--- install-yay.sh
+++ install-yay.sh
@@ -6,7 +6,7 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
