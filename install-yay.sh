95a2be7b435dd46873d06339d00cd928093e05ed
Jiho Lee <optional.int@kakao.com>
2023-09-25 05:19:01 +0900

    feat: add install-yay.sh

---
+++ install-yay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
