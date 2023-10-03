606f61bcd4b3b3d8ea733470c9b8cc05968f1b2f
Jiho Lee <optional.int@kakao.com>
2023-10-03 18:04:35 +0900

    feat: add install-pacman-packages.sh

---
+++ install-pacman-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+sudo pacman -Syu vim neovim wol gcc python curl wget docker docker-compose --noconfirm
