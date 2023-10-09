9ff91c249e0995f966a8e5af74743c54257aa301
Jiho Lee <optional.int@kakao.com>
2023-10-09 18:36:31 +0000

    feat: install vi, man, firefox, chromium using pacman

--- install-pacman-packages.sh
+++ install-pacman-packages.sh
@@ -6,4 +6,4 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
-sudo pacman -Syu vim neovim wol gcc python curl wget docker docker-compose --noconfirm
+sudo pacman -Syu vi vim neovim wol gcc python curl wget docker docker-compose man firefox chromium --noconfirm
