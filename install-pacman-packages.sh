93f1961c65e346ce82739f68a21e747c74a4fd72
Jiho Lee <optional.int@kakao.com>
2023-12-16 15:46:35 +0900

    feat: install powerpill before packman package install

--- install-pacman-packages.sh
+++ install-pacman-packages.sh
@@ -6,4 +6,5 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
+yay -S powerpill
 sudo pacman -Syu vi vim neovim wol gcc python curl wget docker docker-compose man firefox chromium --noconfirm
