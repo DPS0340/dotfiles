b89dc89fd60e2d0c8c219fc786afa3f79eb4a5f8
Jiho Lee <optional.int@kakao.com>
2024-02-17 13:44:53 +0900

    fix: rclnoe mount

--- rclone.sh
+++ rclone.sh
@@ -1,4 +1,4 @@
 mkdir ~/pdf ~/논문 ~/auth > /dev/null 2>&1
-nohup rclone mount onedrive:pdf ~/pdf > /dev/null 2>&1 &
-nohup rclone mount onedrive:논문 ~/논문 > /dev/null 2>&1 &
-nohup rclone mount onedrive:auth ~/auth > /dev/null 2>&1 &
+nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:pdf ~/pdf > /dev/null 2>&1 &
+nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:논문 ~/논문 > /dev/null 2>&1 &
+nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:auth ~/auth > /dev/null 2>&1 &
