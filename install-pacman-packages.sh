4b7d711b177ce6ca8924fe0503dae253d39668f0
Jiho Lee <optional.int@kakao.com>
2024-02-18 23:14:41 +0900

    feat: install downgrade using yay

--- install-pacman-packages.sh
+++ install-pacman-packages.sh
@@ -6,5 +6,5 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
-yay -S powerpill
+yay -S powerpill downgrade
 sudo pacman -Syu vi vim neovim wol gcc python curl wget docker docker-compose man firefox chromium xsel nerd-fonts wine --noconfirm
