f0e116eb077c8a12e94ba97414a7568bacaa9600
Jiho Lee <optional.int@kakao.com>
2024-02-18 17:31:50 +0900

    fix: rclone not working

--- rclone.sh
+++ rclone.sh
@@ -1,4 +1,13 @@
 mkdir ~/pdf ~/논문 ~/auth > /dev/null 2>&1
-nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:pdf ~/pdf > /dev/null 2>&1 &
-nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:논문 ~/논문 > /dev/null 2>&1 &
-nohup rclone mount --default-permissions --vfs-cache-mode full onedrive:auth ~/auth > /dev/null 2>&1 &
+
+sudo umount -lf ~/pdf
+sudo umount -lf ~/논문
+sudo umount -lf ~/auth
+
+sudo chown $USER pdf
+sudo chown $USER 논문
+sudo chown $USER pdf
+
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:pdf ~/pdf &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:논문 ~/논문 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:auth ~/auth &
