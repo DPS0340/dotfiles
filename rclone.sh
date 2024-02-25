31bcfc46fa34bb2924335dfcfc687dc5b53d1649
Jiho Lee <optional.int@kakao.com>
2024-02-25 17:35:12 +0900

    chore: enable debug output on rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
 mkdir ~/pdf ~/논문 ~/auth > /dev/null 2>&1
 
 sudo umount -lf ~/pdf
@@ -8,8 +13,8 @@ sudo chown $USER ~/pdf
 sudo chown $USER ~/논문
 sudo chown $USER ~/auth
 
-touch nohup.out
+touch nohup-rclone.out
 
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:pdf ~/pdf >> ~/dotfiles/nohup.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:논문 ~/논문 >> ~/dotfiles/nohup.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:논문 ~/논문 >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
