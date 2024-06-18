3b6434d8d0e753a1c3dbd8b863656a0e6dff5a40
Jiho Lee <optional.int@kakao.com>
2024-06-18 17:53:25 +0900

    fix: Use nixpkgs-unstable channel, not nixos

--- install-packages.sh
+++ install-packages.sh
@@ -3,7 +3,7 @@
 # Enable debug output
 PS4="\n\033[1;33m>>\033[0m "; set -x
 
-nix-channel --add https://nixos.org/channels/nixos-unstable
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 
 export NIXPKGS_ALLOW_UNFREE=1
 export NIXPKGS_ALLOW_INSECURE=1
