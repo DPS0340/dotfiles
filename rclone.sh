ca5ba2809e1c6af4669bd24721f3aad212e79870
Jiho Lee <optional.int@kakao.com>
2024-08-12 14:37:02 +0900

    fix: rclone.sh not working on macos

--- rclone.sh
+++ rclone.sh
@@ -5,9 +5,9 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 
 mkdir ~/pdf ~/paper ~/auth > /dev/null 2>&1
 
-sudo umount -lf ~/pdf
-sudo umount -lf ~/paper
-sudo umount -lf ~/auth
+sudo umount ~/pdf
+sudo umount ~/paper
+sudo umount ~/auth
 
 sudo chown $USER ~/pdf
 sudo chown $USER ~/paper
@@ -15,6 +15,6 @@ sudo chown $USER ~/auth
 
 touch nohup-rclone.out
 
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --allow-root --default-permissions --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
