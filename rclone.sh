50783c69d8a93a13fc1ab66ca72022b4669fd759
Jiho Lee <optional.int@kakao.com>
2024-11-27 23:53:29 +0900

    feat: Rclone mount by uid, gid

--- rclone.sh
+++ rclone.sh
@@ -13,8 +13,11 @@ sudo chown $USER ~/pdf
 sudo chown $USER ~/paper
 sudo chown $USER ~/auth
 
+UID=$(id -u $USER)
+GID=$(id -g $USER)
+
 touch nohup-rclone.out
 
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --default-permissions --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
+nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
