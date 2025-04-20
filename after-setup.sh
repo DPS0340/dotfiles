2535099fd2a6e2febab162684737964e846eca8c
Jiho Lee <optional.int@kakao.com>
2025-04-20 14:12:49 +0900

    feat: Install lvim on after-setup

--- after-setup.sh
+++ after-setup.sh
@@ -38,3 +38,4 @@ kubectl krew install df-pv
 
 (crontab -l 2>/dev/null; cat $DIR/crontab)| crontab -
 
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
