64744c68439013fc3d0f53a2cd13aa84faf68951
Jiho Lee <optional.int@kakao.com>
2025-04-27 20:17:35 +0900

    feat: Use homeConfigurations by home-manager cli

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,11 @@
 PS4="\n\033[1;33m>>\033[0m "; set -x
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
 
 export NIXPKGS_ALLOW_UNFREE=1
 export NIXPKGS_ALLOW_INSECURE=1
@@ -11,9 +16,6 @@ export NIXPKGS_ALLOW_INSECURE=1
 _OS=$(uname | tr '[:upper:]' '[:lower:]')
 _ARCH=$(uname -m)
 
-flake_attr="legacyPackages.${_ARCH}-${_OS}.homeConfigurations."dps0340".activationPackage"
-
-nix profile install github:dps0340/...s#$flake_attr
-nix profile upgrade $flake_attr
+home-manager switch --flake github:dps0340/...s#dps0340
 
 go install golang.org/x/tools/gopls@latest
