4ebf9cdde13cf8ef2b6c03b6af22fe3929de9355
Jiho Lee <optional.int@kakao.com>
2025-04-28 16:18:02 +0900

    feat: Add darwin-rebuild installation

--- install-packages.sh
+++ install-packages.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Enable debug output
-PS4="\n\033[1;33m>>\033[0m "; set -x
+PS4="\n\033[1;33m>>\033[0m "
+set -x
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
@@ -16,6 +17,15 @@ export NIXPKGS_ALLOW_INSECURE=1
 _OS=$(uname | tr '[:upper:]' '[:lower:]')
 _ARCH=$(uname -m)
 
-home-manager switch --flake github:dps0340/...s#dps0340
+SWITCH_COMMAND="home-manager"
+
+if [ $_OS == "darwin" ]; then
+    SWITCH_COMMAND="darwin-rebuild"
+    if [ $(darwin-rebuild) -ne 0 ]; then
+        nix run nix-darwin/master#darwin-rebuild -- switch
+    fi
+fi
+
+$SWITCH_COMMAND switch --flake github:dps0340/...s#dps0340
 
 go install golang.org/x/tools/gopls@latest
