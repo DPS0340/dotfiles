bd805fb52a637437b65f841f83f4367745f2cf8a
Jiho Lee <optional.int@kakao.com>
2025-05-04 13:42:58 +0900

    feat: Use idempotent mount on rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -5,19 +5,15 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 
 mkdir ~/pdf ~/paper ~/auth > /dev/null 2>&1
 
-umount ~/pdf
-umount ~/paper
-umount ~/auth
-
 chown $USER ~/pdf
 chown $USER ~/paper
 chown $USER ~/auth
 
-UID=$(id -u $USER)
-GID=$(id -g $USER)
+_UID=$(id -u $USER)
+_GID=$(id -g $USER)
 
 touch nohup-rclone.out
 
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
-nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $UID --gid $GID --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
+mountpoint ~/pdf || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full onedrive-personal:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
+mountpoint ~/paper || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full onedrive-personal:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
+mountpoint ~/auth || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full onedrive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
