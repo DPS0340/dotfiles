15e240730517e3d8e9968aaa921eb88a092fbca6
Jiho Lee <optional.int@kakao.com>
2025-07-26 17:50:08 +0900

    feat: Change auth mountpoint to ~/.ssh/remote

--- rclone.sh
+++ rclone.sh
@@ -3,11 +3,11 @@
 # Enable debug output
 PS4="\n\033[1;33m>>\033[0m "; set -x
 
-mkdir ~/pdf ~/paper ~/auth > /dev/null 2>&1
+mkdir ~/pdf ~/paper ~/.ssh/remote > /dev/null 2>&1
 
 chown $USER ~/pdf
 chown $USER ~/paper
-chown $USER ~/auth
+chown $USER ~/.ssh/remote
 
 _UID=$(id -u $USER)
 _GID=$(id -g $USER)
@@ -16,4 +16,4 @@ touch nohup-rclone.out
 
 mountpoint ~/pdf || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:pdf ~/pdf >> ~/dotfiles/nohup-rclone.out 2>&1 &
 mountpoint ~/paper || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:paper ~/paper >> ~/dotfiles/nohup-rclone.out 2>&1 &
-mountpoint ~/auth || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:auth ~/auth >> ~/dotfiles/nohup-rclone.out 2>&1 &
+mountpoint ~/.ssh/remote || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:auth ~/.ssh/remote >> ~/dotfiles/nohup-rclone.out 2>&1 &
