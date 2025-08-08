9cfb72903e5ce5d452bb797dbe1d4f415d6cb3ac
Jiho Lee <optional.int@kakao.com>
2025-08-08 11:00:59 +0900

    feat: Increase rclone mount cache age and poll interval

--- rclone.sh
+++ rclone.sh
@@ -16,6 +16,6 @@ _GID=$(id -g $USER)
 
 touch ~/nohup-rclone.out
 
-( mount | grep ~/pdf ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:pdf ~/pdf >> ~/nohup-rclone.out 2>&1 & disown
-( mount | grep ~/paper ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:paper ~/paper >> ~/nohup-rclone.out 2>&1 & disown
-( mount | grep ~/.ssh/remote ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full google-drive:auth ~/.ssh/remote >> ~/nohup-rclone.out 2>&1 & disown
+( mount | grep ~/pdf ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full --dir-cache-time 1y --poll-interval 1h --vfs-cache-max-age 1y --vfs-cache-min-free-space 10G --vfs-cache-poll-interval 1h google-drive:pdf ~/pdf >> ~/nohup-rclone.out 2>&1 & disown
+( mount | grep ~/paper ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full --dir-cache-time 1y --poll-interval 1h --vfs-cache-max-age 1y --vfs-cache-min-free-space 10G --vfs-cache-poll-interval 1h google-drive:paper ~/paper >> ~/nohup-rclone.out 2>&1 & disown
+( mount | grep ~/.ssh/remote ) || nohup rclone mount --allow-non-empty --log-level ERROR --allow-other --uid $_UID --gid $_GID --vfs-cache-mode full --dir-cache-time 1y --poll-interval 1h --vfs-cache-max-age 1y --vfs-cache-min-free-space 10G --vfs-cache-poll-interval 1h google-drive:auth ~/.ssh/remote >> ~/nohup-rclone.out 2>&1 & disown
