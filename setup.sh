653229d081dc6b30dec47f0a3a4445cca238a431
Jiho Lee <optional.int@kakao.com>
2025-08-14 16:40:10 +0900

    feat: Install gopls on setup.sh

--- setup.sh
+++ setup.sh
@@ -68,6 +68,8 @@ sdk install gradle 7.6
 pyenv install 3.10.9
 pyenv global 3.10.9
 
+go install golang.org/x/tools/gopls@latest
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
